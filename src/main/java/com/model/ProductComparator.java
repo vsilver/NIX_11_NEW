ee116fe7913bf717d3528977a0bcdd954c356cd3
vsilvanovych <valeriysilvanovich@gmail.com>
2022-07-23 17:44:44 +0300

    Linked list + Comparator correct

--- src/main/java/com/model/ProductComparator.java
+++ src/main/java/com/model/ProductComparator.java
@@ -6,15 +6,6 @@ import java.util.Comparator;
 
 public class ProductComparator <T extends Product> implements Comparator<T> {
     @Override
-    /*public int compare(T first, T second) {
-        if (second.getPrice() == first.getPrice()) {
-            if (first.getTitle().equals(second.getTitle())) {
-                return Integer.compare(first.getCount(), second.getCount());
-            }
-            return first.getTitle().compareTo(second.getTitle());
-        }
-        return Double.compare(second.getPrice(), first.getPrice());
-    }*/
     public int compare(T o1, T o2) {
         if (o2.getPrice() == o1.getPrice()) {
             if (o1.getTitle().equals(o2.getTitle())) {
